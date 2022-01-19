f6fb39ea4902dffdd588275b06ac16fee54d34fb
shlyapnik <77507745+shlyapnik@users.noreply.github.com>
2022-01-19 22:38:51 +0500

    Add files via upload
    php файлы

---
+++ index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Вёрстка, программирование сайтов | Михаил Рыбьянов</title>
+    <meta name="description" content="Создание сайтов по всему Узбекистану. Низкие цены, гарантия качества. Обращайтесь!">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">    
+</head>
+<body>
+    <section id="main">
+        <header id="header">
+            <div class="wrapper">
+                <div class="row">
+                    <div class="social col">
+                        <a href="https://github.com/" class="icon" style="background: url('img/github.svg') center center no-repeat;"></a>
+                        <a href="https://twitter.com/" class="icon" style="background: url('img/twitter.svg') center center no-repeat;"></a>
+                        <a href="https://www.instagram.com/" class="icon" style="background: url('img/instagram.svg') center center no-repeat;"></a>
+                        <a href="http://facebook.com/" class="icon" style="background: url('img/facebook.svg') center center no-repeat;"></a>
+                    </div>
+                    <div class="hero_text col">
+                        <div class="freelancer">Фрилансер</div>
+                        <h1>Михаил Рыбьянов</h1>
+                        <p>Разрабатываю сайты для малого и среднего бизнеса. У меня можно заказать дизайн и верстку небольшого сайта или интернет-магазина</p>
+                        <div class="button yellow">
+                            <a class="open_popup" href="#">Бесплатная консультация</a>
+                        </div>
+                    </div>
+                    <div class="hero_img col">
+                       <div class="img" style="background: url('images/avatar.jpg') center center no-repeat; background-size: cover;"></div>
+                    </div>              
+                </div>
+            </div>
+        </header>
+        <section id="benefits">
+            <div class="wrapper">
+                <h2>Со мной удобно работать</h2>
+                <div class="row">
+                    <div class="benefit col">
+                        <img src="img/offer-img.svg" alt="По договору">
+                        <h3>По договору</h3>
+                        <p>Предоставлю все документы для бухгалтерии: договоры, счета и акты. Работаю как ИП без НДС.</p>
+                    </div>
+                    <div class="benefit col">
+                        <img src="img/deadline-img.svg" alt="Точно в срок">
+                        <h3>Точно в срок</h3>
+                        <p>В договоре прописана моя  ответственность за срок проекта. Если просрочу — плачу неустойку.</p>
+                    </div>
+                    <div class="benefit col">
+                        <img src="img/agreement-img.svg" alt="Ценник под вас">
+                        <h3>Ценник под вас</h3>
+                        <p>Если у вас нетиповой проект, сделаю рассчет цены за работу и объясню, как складывается цена.</p>
+                    </div>
+                    <div class="benefit col">
+                        <img src="img/steps-img.svg" alt="Премежуточные итоги">
+                        <h3>Премежуточные итоги</h3>
+                        <p>Показываю работу поэтапно, а не в последий момент перед крайним сроком.</p>
+                    </div>
+                    <div class="benefit col">
+                        <img src="img/storage-img.svg" alt="Хостинг и домен">
+                        <h3>Хостинг и домен</h3>
+                        <p>Если мы договорились о цене проекта, в неё входит всё: домен и хостинг на год + SSL сертификат.</p>
+                    </div>
+                    <div class="benefit col">
+                        <img src="img/conversation-img.svg" alt="Не брошу после сдачи">
+                        <h3>Не брошу после сдачи</h3>
+                        <p>Научу вас или вашего сотрудника пользоваться сайтом, запишу видео-инструкции.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- <section id="portfolio">
+            <div class="wrapper">
+                <h2>Последние проекты</h2>
+                <div class="row slider">   
+                    <div class="project col active">
+                        <div class="row">
+                            <div class="project_img col" style="background: url('images/project-landing.jpg') center center no-repeat; background-size: cover;"></div>
+                            <div class="project_text col">
+                                <h3>Лендинг с 3D фигурами</h3>
+                                <div class="row">
+                                    <div class="time col">
+                                        <h4>Сроки</h4>
+                                        <p>4 рабочих дня</p>
+                                    </div>
+                                    <div class="cost col">
+                                        <h4>Стоимость</h4>
+                                        <p>4000 рублей</p>
+                                    </div>
+                                    <div class="feedback col">
+                                        <h4>Отзыв клиента</h4>
+                                        <p>Делаем с Михаилом уже не первый проект. Радует, что Михаил всегда на связи и дает рекомендации всегда по делу. Буду обращаться еще.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div class="project col">
+                        <div class="row">
+                            <div class="project_img col" style="background: url('images/project-blog.jpg') center center no-repeat; background-size: cover;"></div>
+                            <div class="project_text col">
+                                <h3>Блог о строительстве BlogAboutStroy</h3>
+                                <div class="row">
+                                    <div class="time col">
+                                        <h4>Сроки</h4>
+                                        <p>7 рабочих дней</p>
+                                    </div>
+                                    <div class="cost col">
+                                        <h4>Стоимость</h4>
+                                        <p>7400 рублей</p>
+                                    </div>
+                                    <div class="feedback col">
+                                        <h4>Отзыв клиента</h4>
+                                        <p>Мне понравилось работать с Михаилом, потому что он все время был на связи и предлагал свои решения, когда мы заходили в тупик с дизайном. Мне нужны были закрывающие документы для бухгалтерии, и Михаил — тот редкий фрилансер, у которого с документами все в порядке</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div id="btn-prev"></div> 
+                    <div id="btn-next"></div>
+                </div>
+                <div class="row dots">
+                    <div class="dot col active"></div>
+                    <div class="dot col"></div>
+                </div>
+            </div>            
+        </section> -->
+        <section id="portfolio">
+            <div class="wrapper swiper swiper-container">
+                <h2>Последние проекты</h2>
+                <div class="row swiper-wrapper">   
+                    <div class="project col swiper-slide">
+                        <div class="row">
+                            <div class="project_img col" style="background: url('images/project-landing.jpg') center center no-repeat; background-size: cover;"></div>
+                            <div class="project_text col">
+                                <h3>Лендинг с 3D фигурами</h3>
+                                <div class="row">
+                                    <div class="time col">
+                                        <h4>Сроки</h4>
+                                        <p>4 рабочих дня</p>
+                                    </div>
+                                    <div class="cost col">
+                                        <h4>Стоимость</h4>
+                                        <p>4000 рублей</p>
+                                    </div>
+                                    <div class="feedback col">
+                                        <h4>Отзыв клиента</h4>
+                                        <p>Делаем с Михаилом уже не первый проект. Радует, что Михаил всегда на связи и дает рекомендации всегда по делу. Буду обращаться еще.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div class="project col swiper-slide">
+                        <div class="row">
+                            <div class="project_img col" style="background: url('images/project-blog.jpg') center center no-repeat; background-size: cover;"></div>
+                            <div class="project_text col">
+                                <h3>Блог о строительстве BlogAboutStroy</h3>
+                                <div class="row">
+                                    <div class="time col">
+                                        <h4>Сроки</h4>
+                                        <p>7 рабочих дней</p>
+                                    </div>
+                                    <div class="cost col">
+                                        <h4>Стоимость</h4>
+                                        <p>7400 рублей</p>
+                                    </div>
+                                    <div class="feedback col">
+                                        <h4>Отзыв клиента</h4>
+                                        <p>Мне понравилось работать с Михаилом, потому что он все время был на связи и предлагал свои решения, когда мы заходили в тупик с дизайном. Мне нужны были закрывающие документы для бухгалтерии, и Михаил — тот редкий фрилансер, у которого с документами все в порядке</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+                <!-- Добавляем если нам нужны стрелки управления -->
+				<!-- <div class="swiper-button-prev"></div>
+				<div class="swiper-button-next"></div> -->
+				<!-- Добавляем, если нам нужна пагинация -->
+				<div class="swiper-pagination"></div>
+            </div>            
+        </section>
+        <section id="contact">
+            <div class="wrapper">
+                <h2>Свяжитесь со мной</h2>
+                <div class="row">
+                    <div class="contact_form col">
+                        <h4>Можете просто оставить заявку, я позвоню вам сегодня</h4>
+                        <form action="/">
+                            <div class="input_field name">
+                                <input type="text" name="name" placeholder="Ваше имя">
+                            </div>
+                            <div class="input_field phone">
+                                <input type="text" name="phone" placeholder="Ваш телефон">
+                            </div>
+                            <div class="button">
+                                <input type="submit" value="Бесплатная консультация">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="contact_text col">
+                        <div class="row">
+                            <div class="contact_phone col">
+                                <h3>Телефон</h3>
+                                +998 93 724 78 11
+                            </div>
+                            <div class="contact_email col">
+                                <h3>Почта</h3>
+                                mace@mail.ru
+                            </div>
+                            <div class="contact_social col">
+                                <h3>Соцсети</h3>
+                                <a href="https://t.me/Artraven">t.me/Artraven</a>
+                                <a href="https://vk.me/Artraven">vk.me/Artraven</a>
+                                <a href="https://fb.me/Artraven">fb.me/Artraven</a>
+                            </div>
+                            <div class="social col">
+                                <a href="https://github.com/" class="icon" style="background: url('img/github.svg') center center no-repeat;"></a>
+                                <a href="https://twitter.com/" class="icon" style="background: url('img/twitter.svg') center center no-repeat;"></a>
+                                <a href="https://www.instagram.com/" class="icon" style="background: url('img/instagram.svg') center center no-repeat;"></a>
+                                <a href="http://facebook.com/" class="icon" style="background: url('img/facebook.svg') center center no-repeat;"></a>
+                            </div>
+                            <div class="confidence col">
+                                <a href="#">Политика конфиденциальности</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>    
+        <footer id="footer">
+            <div class="wrapper">                
+                <div class="row">
+                    
+                </div>
+            </div>
+        </footer>
+    </section>
+    <div class="popup_bg">
+        <div class="popup">
+            <div class="close_popup"></div>    
+            <h4>Можете просто оставить заявку, я позвоню вам сегодня</h4> 
+            <form action="#">                       
+                <div class="input_field name">
+                    <input type="text" name="name" placeholder="Ваше имя">
+                </div>
+                <div class="input_field phone">
+                    <input type="text" name="phone" placeholder="Ваш телефон">
+                </div>
+                <div class="button">
+                    <input type="submit" value="Отправить">
+                </div>
+            </form>
+        </div>        
+    </div>
+    <script src="js/scripts.min.js"></script>
+</body>
+</html>
